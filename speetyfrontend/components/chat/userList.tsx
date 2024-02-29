2d7b404f28e95c9ba6f0d9b53ec22d1a603aa0d0
rishikeshydv <rishikeshadh4@gmail.com>
2024-02-29 00:31:24 -0500

    feat: user chatprop updated

--- speetyfrontend/components/chat/userList.tsx
+++ speetyfrontend/components/chat/userList.tsx
@@ -1,34 +1,35 @@
-import React, { useEffect } from 'react';
-import ChatListProp from '@/services/chat/ChatListProp';
+import React, { useEffect } from "react";
+import ChatListProp from "@/services/chat/ChatListProp";
 //import all the packages and modules that would connect it to firebase
 
 export default function UserList() {
-
-  TODO: //use the following code to use document and access the element by Id
+  //use the following code to use document and access the element by Id
   // useEffect(() => {
   //   const userSpace = document.getElementById('all_users')
   // },[]);
-  
-  return (
-    <div id="all_users">
+
+  TODO: return (
+    <div className="bg-gray-100 rounded-3xl ">
       {/* {inside these curly braces, write the logic for getting all the getDocs of the users
         and then forEach, add a ChatListProp
       } */}
       {/* For now I will be haardcoding the users list UI */}
-      <ChatListProp 
-      imgUrl = "123.png"
-      userName = "Rishi"
-      lastMsg = "Hi!"
-      lastMsgTime = "10:30 AM"
-      newMsg = {true}
+
+      <ChatListProp
+        imgUrl="123.png"
+        userName="Rishi"
+        lastMsg="Hi!"
+        lastMsgTime="10:30 AM"
+        newMsg={true}
       />
-            <ChatListProp 
-      imgUrl = "456.png"
-      userName = "Saddy"
-      lastMsg = "Hello"
-      lastMsgTime = "11:00PM"
-      newMsg = {false}
+
+      <ChatListProp
+        imgUrl="456.png"
+        userName="Saddy"
+        lastMsg="Hello"
+        lastMsgTime="11:00PM"
+        newMsg={false}
       />
     </div>
-  )
+  );
 }
--- speetyfrontend/services/chat/ChatListProp.tsx
+++ speetyfrontend/services/chat/ChatListProp.tsx
@@ -1,34 +1,45 @@
-import React from 'react';
+import React from "react";
 
 interface ChatList {
-    imgUrl: string;
-    userName: string;
-    lastMsg: string;
-    lastMsgTime: string;
-    newMsg: boolean;
+  imgUrl: string;
+  userName: string;
+  lastMsg: string;
+  lastMsgTime: string;
+  newMsg: boolean;
 }
 
-const ChatListProp: React.FC<ChatList> = ({ imgUrl, userName, lastMsg, lastMsgTime, newMsg }) => {
-    if (newMsg) {
-        return (
-            <div className='bg-blue-100 rounded-2xl'>
-                <img src="/user.png" alt="user1" className='w-12 h-12' />
-                <h1 className='font-bold text-2xl'>{userName}</h1>
-                <h3>New Message</h3>
-            </div>
-        );
-    } else {
-        return (
-            <div className='flex bg-gray-100 rounded-2xl'>
-                <img src="/user.png" alt="user1" className='w-12 h-12' />
-                <div className='ml-5'>
-                <h1 className='font-bold text-2xl'>{userName}</h1>
-                <h3>{lastMsg}</h3>
-                </div>  
-                <h3 className='mt-5 ml-5'>{lastMsgTime}</h3>
-            </div>
-        );
-    }
+const ChatListProp: React.FC<ChatList> = ({
+  imgUrl,
+  userName,
+  lastMsg,
+  lastMsgTime,
+  newMsg,
+}) => {
+  if (newMsg === true) {
+    return (
+      <div className="flex bg-blue-100 w-80 h-20 rounded-2xl mt-5 py-2">
+        <img src="/user.png" alt="user1" className="w-12 h-12" />
+        <div className="flex flex-col ml-5">
+        <h1 className="font-bold text-2xl">{userName}</h1>
+        <h3>New Message</h3>
+        </div>
+
+      </div>
+    );
+  } else {
+    return (
+      <div style={{ backgroundColor: '#FFFFFF' }}  className="flex w-80 h-20 rounded-2xl mt-5 py-5">
+        <img src="/user.png" alt="user1" className="w-12 h-12" />
+        <div className="ml-5">
+          <div className="flex flex-row mt-3">
+            <h1 className="font-bold text-2xl">{userName}</h1>
+            <h3 className="ml-5 py-1">{lastMsgTime}</h3>
+          </div>
+          <h3>{lastMsg}</h3>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default ChatListProp;
