a1b93f089ac7d2a1b0974262f328d68d1e0cf23a
rishikeshydv <rishikeshadh4@gmail.com>
2024-03-05 10:47:40 -0500

    feat: agent prop

--- speetyfrontend/app/agent/page.tsx
+++ speetyfrontend/app/agent/page.tsx
@@ -1,27 +1,26 @@
-"use client"
-import React,{useState} from 'react'
+"use client";
+import React, { useState } from "react";
 import poppins from "@/font/font";
-import SearchBar from '@/components/SearchBar';
-import PropertyProp from '@/services/property/PropertyProp';
-
+import SearchBar from "@/components/SearchBar";
+import AgentProp from "@/services/agent/AgentProp";
 
 export default function Property() {
-    //write a logic to run a query on the database and retrieve the results
-    //use the PropertyProp for each returns
+  //write a logic to run a query on the database and retrieve the results
+  //use the PropertyProp for each returns
   return (
     <div className={poppins.className}>
-        <SearchBar/>
-        <div className='mt-4 ml-4'>
-        <PropertyProp 
-         videoUrl= '/house.jpeg'
-         price= '$100,000'
-         beds= '5'
-         baths= '3'
-         houseType= 'Condo'
-         address= '43 Roseland Ave, Caldwell, NJ, 07006'
-         listedBy= 'Speety Realty Office'
+      <SearchBar />
+      <div className="mt-4 ml-4">
+        <AgentProp
+          photoUrl={"/agent.jpeg"}
+          name={"Roman Witek"}
+          stars={4}
+          phone={"(973) 9739-739"}
+          usersReviews={258}
+          company={"Coldwell Banker Realty"}
+          license={"7844026"}
         />
-        </div>
+      </div>
     </div>
-  )
+  );
 }
--- speetyfrontend/services/agent/AgentProp.tsx
+++ speetyfrontend/services/agent/AgentProp.tsx
@@ -1,26 +1,28 @@
-import React from 'react'
-
+import React, { useEffect } from 'react';
+import Rating from '@mui/material/Rating';
+import { AiFillPhone } from "react-icons/ai";
 interface Agent{
     photoUrl:string;    //we wont be using image like zillow, we will use GIF/Video
     name:string;
     stars:number;
     phone:string;
-    usersDealt:string
+    usersReviews:number;
     company: string;
     license: string;
 
 }
-const AgentProp:React.FC<Agent> = ({photoUrl,name,stars,phone,usersDealt,company,license}) => {
-
+const AgentProp:React.FC<Agent> = ({photoUrl,name,stars,phone,usersReviews,company,license}) => {
   return (
     <div className='border-2 shadow-md rounded-2xl h-80 w-96 bg-gray-600'>
         <img src={photoUrl} alt="houseVideo" className='rounded-2xl'/>
-        <p className='text-2xl font-bold mt-2'>{price}</p>
-        <p className='text-xl'>{beds} bds | {baths} ba | {houseType}</p>
-        <p className='text-xl'>{address}</p>
-        <p>{listedBy}</p>
+        <p className='text-2xl font-bold mt-2 ml-4'>{name}</p>
+        <div className='flex ml-2'><AiFillPhone className='h-8 w-8' /><p className='text-xl mt-1'> {phone}</p></div>
+        <p className='ml-2'><Rating name="read-only" value={stars} readOnly /></p>
+        <p className='text-xl ml-2'>{usersReviews} Reviews</p>
+        <p className='ml-2'>{company}</p>
+        <p className='ml-2' >Agent License #: {license}</p>
     </div>
   )
 }
 
-export default PropertyProp;
+export default AgentProp;
